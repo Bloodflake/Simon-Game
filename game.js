999db2292a4349b4d17276f20b4c456ba8269935
sumit patil <sumitpatil90990@gmail.com>
2021-02-24 02:29:18 +0530

    added functionality for generating and recording pattern

--- game.js
+++ game.js
@@ -1,1 +1,33 @@
-alert("hello")
+var buttonColours = ["red", "blue", "green", "yellow"];
+var gamPattern = [];
+var userClickedPattern = [];
+
+function nextSequence(){
+  var randomNumber = Math.floor(Math.random()*4);
+  var randomChosenColour = buttonColours[randomNumber];
+  gamPattern.push(randomChosenColour);
+  $("#" + randomChosenColour).fadeOut(100).fadeIn(100).fadeOut(100).fadeIn(100);
+  playSound(randomChosenColour);
+}
+
+$(".btn").click(function(){
+  var userChosenColour = $(this).attr("id");
+  animatePress(userChosenColour);
+
+  userClickedPattern.push(userChosenColour);
+  playSound(userChosenColour);
+
+  console.log(userClickedPattern);
+});
+
+function playSound(name){
+  var audio = new Audio("sounds/" + name + ".mp3");
+  audio.play();
+}
+
+function animatePress(currentColour){
+  $("." + currentColour).addClass("pressed");
+  setTimeout(function(){
+    $("#" + currentColour).removeClass("pressed");
+  },100);
+}
